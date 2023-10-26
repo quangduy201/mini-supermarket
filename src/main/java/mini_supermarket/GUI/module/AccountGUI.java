50c7c44ddbd254bb1ff4ec906ec8c648217ea37e
quangduy201 <dinhduy2012003@gmail.com>
2023-10-26 18:48:01 +0700

    test DataTable

--- src/main/java/mini_supermarket/GUI/module/AccountGUI.java
+++ src/main/java/mini_supermarket/GUI/module/AccountGUI.java
@@ -1,9 +1,14 @@
 package mini_supermarket.GUI.module;
 
+import mini_supermarket.BLL.AccountBLL;
+import mini_supermarket.DTO.Account;
 import mini_supermarket.DTO.Function;
+import mini_supermarket.GUI.component.DataTable;
 import mini_supermarket.GUI.component.RoundPanel;
 import mini_supermarket.GUI.layout.ControlLayout;
 import mini_supermarket.GUI.layout.LeftRightLayout;
+import mini_supermarket.utils.Log;
+import mini_supermarket.utils.__;
 
 import java.util.List;
 
@@ -17,5 +22,21 @@ public class AccountGUI extends ControlLayout {
         panelFunction = getTopPanel();
 
         panelData = getBottomPanel();
+        
+        AccountBLL accountBLL = new AccountBLL();
+        List<Account> accounts = accountBLL.findAll();
+        panelData.add(new DataTable(
+            accountBLL.getData(accounts,
+                __.ACCOUNT.COLUMN.USERNAME,
+                __.ROLE.COLUMN.NAME,
+                __.STAFF.COLUMN.NAME,
+                __.STAFF.COLUMN.GENDER
+            ), new Object[]{
+                "Tên tài khoản",
+                "Tên chức vụ",
+                "Tên nhân viên",
+                "Giới tính"
+            }, e -> Log.debug(AccountGUI.class, "Test")
+        ));
     }
 }
